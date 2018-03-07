1a0cf7dc9db3bbe2463ede6d9cb4e473f96778fe
Mubarak Muse <musex025@umn.edu>
2018-03-06 19:11:10 -0600

    moving to cse lab machines

--- iteration1/src/arena.cc
+++ iteration1/src/arena.cc
@@ -136,7 +136,7 @@ void Arena::UpdateEntitiesTimestep() {
     for (auto &ent2 : entities_) {
       if (ent2 == ent1) { continue; }
       if (IsColliding(ent1, ent2)) {
-        AdjustEntityOverlap(ent1, ent2);
+        //AdjustEntityOverlap(ent1, ent2);
         ent1->HandleCollision(ent2->get_type(), ent2);
         if (ent2->get_type() == kObstacle && ent1->get_type() == kRobot){
           robot_->lose_A_Life();
