02eb98bbeba4b76e55345cf52e27b73623067e71
Mubarak Muse <musex025@umn.edu>
2018-04-08 22:02:11 -0500

    Feat(motionhandlerrobot,explore,coward,aggressive): issue #12 observer strategy
    - made classes that inherit from motiion handler
    - so far they are indentical to the motion handler robot love that i have already made

---
+++ iteration2/src/motion_handler_robot_aggressive.h
@@ -0,0 +1,90 @@
+/**
+ * @file motion_handler_robot_aggressive.h
+ *
+ * @copyright 2018 3081 Staff, All rights reserved.
+ */
+
+#ifndef SRC_MOTION_HANDLER_ROBOT_AGGRESSIVE_H_
+#define SRC_MOTION_HANDLER_ROBOT_AGGRESSIVE_H_
+
+/*******************************************************************************
+ * Includes
+ ******************************************************************************/
+#include <cassert>
+#include <iostream>
+
+#include "src/common.h"
+#include "src/motion_handler.h"
+#include "src/sensor_touch.h"
+#include "src/communication.h"
+#include "src/robot_type.h"
+/*******************************************************************************
+ * Namespaces
+ ******************************************************************************/
+NAMESPACE_BEGIN(csci3081);
+
+/*******************************************************************************
+ * Classes
+ ******************************************************************************/
+
+/**
+ * @brief Class managing a Robot's and Light's speed and heading angle based
+ * on collisions and user inputs.
+ *
+ */
+class MotionHandlerRobotAggressive : public MotionHandler {
+ public:
+  explicit MotionHandlerRobotAggressive(ArenaMobileEntity * ent)
+      : MotionHandler(ent) {}
+
+  MotionHandlerRobotAggressive(const MotionHandlerRobotAggressive& other) = default;
+  MotionHandlerRobotAggressive& operator=(const MotionHandlerRobotAggressive& other) = default;
+
+  /**
+  * @brief Update the speed and the pose angle according to the sensor readings.
+  *
+  */
+  void UpdateVelocity() override;
+
+  /**
+   * @brief Increase the overall speed of the entity by speed_delta.
+   */
+  void IncreaseSpeed() override;
+
+  /**
+   * @brief Decrease the overall speed of the entity by speed_delta.
+   */
+  void DecreaseSpeed() override;
+
+  /**
+   * @brief Turn the entity to the right by angle_delta (in degrees?)
+   */
+  void TurnRight() override;
+
+  /**
+   * @brief Turn the entity to the left by angle_delta (in degrees?)
+   */
+  void TurnLeft() override;
+
+  /**
+   * @brief stop the entity to be at zero velocity.
+   */
+  void Stop() override;
+
+  // void update(RobotType rt, double reading){
+  //   robot_type_= rt;
+  //   total_reading_ = reading;
+  // }
+  // RobotType get_robot_type(){
+  //   return robot_type_;
+  // }
+
+ private:
+  double clamp_vel(double vel);
+  // RobotType robot_type_;
+  // double total_reading_;
+};
+
+NAMESPACE_END(csci3081);
+
+#endif  // SRC_MOTION_HANDLER_ROBOT_AGGRESSIVE_H_
---
+++ iteration2/src/motion_handler_robot_coward.cc
@@ -0,0 +1,84 @@
+/**
+ * @file motion_handler_robot_coward.cc
+ *
+ * @copyright 2018 3081 Staff, All rights reserved.
+ */
+
+/*******************************************************************************
+ * Includes
+ ******************************************************************************/
+#include "src/motion_handler_robot_coward.h"
+#include "src/motion_behavior_differential.h"
+#include "src/robot.h"
+#include "src/robot_type.h"
+
+/*******************************************************************************
+ * Namespaces
+ ******************************************************************************/
+NAMESPACE_BEGIN(csci3081);
+
+/*******************************************************************************
+ * Member Functions
+ ******************************************************************************/
+// @TODO add clamped
+
+void MotionHandlerRobotCoward::TurnLeft() {
+  set_velocity(
+    get_velocity().left  - get_angle_delta(),
+    get_velocity().right + get_angle_delta());
+}
+
+void MotionHandlerRobotCoward::TurnRight() {
+  set_velocity(
+    get_velocity().left  + get_angle_delta(),
+    get_velocity().right - get_angle_delta());
+}
+
+void MotionHandlerRobotCoward::IncreaseSpeed() {  // added for priorty
+  if ((get_velocity().left + get_speed_delta()) <= ROBOT_MAX_SPEED
+    && (get_velocity().right + get_speed_delta()) <= ROBOT_MAX_SPEED) {
+     set_velocity(
+    get_velocity().left  + get_speed_delta(),
+    get_velocity().right + get_speed_delta());
+  }
+}
+void MotionHandlerRobotCoward::DecreaseSpeed() {  // added for priorty
+    if (0.00 <=( get_velocity().left - get_speed_delta())
+    && 0.00 <= (get_velocity().right - get_speed_delta())) {
+      set_velocity(
+    get_velocity().left  - get_speed_delta(),
+    get_velocity().right - get_speed_delta());
+    }
+  }
+void MotionHandlerRobotCoward::Stop() {  // added for priorty 1 iteration 1
+  set_velocity(0.00, 0.00);
+}
+
+void MotionHandlerRobotCoward::UpdateVelocity() {
+  if (entity_->get_touch_sensor()->get_output()) {
+     entity_->RelativeChangeHeading(+180);
+  }
+  // std::cout << get_sensor_reading()*.0027<< "      ";
+  set_velocity(
+    get_right_sensor_reading()*.0027,
+    get_left_sensor_reading()*.0027);
+}
+
+double MotionHandlerRobotCoward::clamp_vel(double vel) {
+  // @TODO: don't go backwards
+  double clamped = 0.0;
+  if (vel > 0) {
+    clamped = (vel > get_max_speed()) ?
+              get_max_speed():
+              vel;
+  } else {
+    clamped = (vel < -get_max_speed()) ?
+              -get_max_speed():
+              vel;
+  }
+  return clamped;
+} /* clamp_vel() */
+
+
+
+NAMESPACE_END(csci3081);
