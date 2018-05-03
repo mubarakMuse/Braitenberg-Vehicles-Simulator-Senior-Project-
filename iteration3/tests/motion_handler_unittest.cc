ce56b1b6cae2ee1d68f6a11a1385f8a5c9444eaa
Mubarak Muse <musex025@umn.edu>
2018-05-03 17:12:04 -0500

    motionHandler tests

--- iteration3/tests/motion_handler_unittest.cc
+++ iteration3/tests/motion_handler_unittest.cc
@@ -13,6 +13,7 @@
 #include "src/light_sensor.h"
 #include "src/sensor.h"
 #include "src/arena_entity.h"
+#include "src/wheel_velocity.h"
 
 #ifdef MOTIONHANDLER_TESTS
 
@@ -23,41 +24,102 @@ class MotionHandlerTest : public ::testing::Test {
  protected:
   virtual void SetUp() {
     explore_robot = new csci3081::Robot(csci3081::kExplore);
-    explore_robot->set_radius(10);
-    // explore_robot.set_pose(100,100,0);
+    explore_robot->set_radius(16);
+  
 
     coward_robot = new csci3081::Robot(csci3081::kCoward);
-    coward_robot->set_radius(10);
-    // coward_robot.set_pose(100,100,0);
+    coward_robot->set_radius(16);
+   
   }
   csci3081::Robot * explore_robot;
   csci3081::Robot * coward_robot;
-  std::vector<class ArenaEntity *> light_entities_;
+  std::vector<class csci3081::ArenaEntity *> light_entities_;
 };
 
 /*******************************************************************************
  * Test Cases
  ******************************************************************************/
 
-// TEST_F(MotionHandlerTest,onecloselight) {
-//   explore_robot.set_pose(100,100,0);
-//   explore_robot.UpdateSensorPoses();
-//   Light * light1 = new csci3081::Light();
-//   light1->set_pose(100,100,0);
-//   light_entities_.clear();
-//   light_entities_.push_back(ligh1);
-//   get_right_light_sensor()->set_reading(0);
-//   get_right_light_sensor()->update(light_entities_);
-//   get_left_light_sensor()->set_reading(0);
-//   get_left_light_sensor()->update(light_entities_);
-//   csci3081::WheelVelocity v = explore_robot->get_motion_handler().get_velocity();
-//   explore_robot.TimestepUpdate(1);
-//   csci3081::WheelVelocity v = explore_robot->get_motion_handler().get_velocity();
-//   EXPECT_EQ(std::make_tuple(v.left, v.right), std::make_tuple(0, 0)) << "Fail: Robot did not stop on collision with an Obstacle";
-//   explore_robot.TimestepUpdate(1);
+ TEST_F(MotionHandlerTest,OnelightApproching) {
+  csci3081::Light * light1 = new csci3081::Light();
+  csci3081::Pose Rpose = { 100, 100, 0};
+  csci3081::Pose Lposebefore = { 300, 300, 0};
+  csci3081::Pose Lposeafter = { 200, 200, 0};
+  light_entities_.clear();
+  light_entities_.push_back(light1);
+  explore_robot->set_pose(Rpose);
+  explore_robot->UpdateSensorPoses();
+  coward_robot->set_pose(Rpose);
+  coward_robot->UpdateSensorPoses();
+  
+  light1->set_pose(Lposebefore);
+  explore_robot->get_left_light_sensor()->update(light_entities_);
+  explore_robot->get_right_light_sensor()->update(light_entities_);
+  coward_robot->get_left_light_sensor()->update(light_entities_);
+  coward_robot->get_right_light_sensor()->update(light_entities_);
+  explore_robot->TimestepUpdate(1);
+  coward_robot->TimestepUpdate(1);
 
-//   EXPECT_EQ(get_right_light_sensor()->get_pose(),Pose(108,106,0));
-//   EXPECT_EQ(get_left_light_sensor()->get_pose(),Pose(108,-106,0));
-//  }
+  csci3081::WheelVelocity ve_before = explore_robot->get_motion_handler()->get_velocity();
+  csci3081::WheelVelocity vc_before = coward_robot->get_motion_handler()->get_velocity();
+  
+  light1->set_pose(Lposeafter);
+  explore_robot->get_left_light_sensor()->update(light_entities_);
+  explore_robot->get_right_light_sensor()->update(light_entities_);
+  coward_robot->get_left_light_sensor()->update(light_entities_);
+  coward_robot->get_right_light_sensor()->update(light_entities_);
+  explore_robot->TimestepUpdate(1);
+  coward_robot->TimestepUpdate(1);
+
+  csci3081::WheelVelocity ve_after = explore_robot->get_motion_handler()->get_velocity();
+  csci3081::WheelVelocity vc_after = coward_robot->get_motion_handler()->get_velocity();
+
+  EXPECT_GT(ve_before.left,ve_after.left);
+  EXPECT_GT(ve_before.right,ve_after.right);
+  EXPECT_LT(vc_before.left,vc_after.left);
+  EXPECT_LT(vc_before.right,vc_after.right);
+  
+}
+
+ TEST_F(MotionHandlerTest,OneLightMovingAway) {
+  csci3081::Light * light1 = new csci3081::Light();
+  csci3081::Pose Rpose = { 100, 100, 0};
+  csci3081::Pose Lposebefore = { 100, 100, 0};
+  csci3081::Pose Lposeafter = { 200, 200, 0};
+  light_entities_.clear();
+  light_entities_.push_back(light1);
+  explore_robot->set_pose(Rpose);
+  explore_robot->UpdateSensorPoses();
+  coward_robot->set_pose(Rpose);
+  coward_robot->UpdateSensorPoses();
+  
+  light1->set_pose(Lposebefore);
+  explore_robot->get_left_light_sensor()->update(light_entities_);
+  explore_robot->get_right_light_sensor()->update(light_entities_);
+  coward_robot->get_left_light_sensor()->update(light_entities_);
+  coward_robot->get_right_light_sensor()->update(light_entities_);
+  explore_robot->TimestepUpdate(1);
+  coward_robot->TimestepUpdate(1);
+
+  csci3081::WheelVelocity ve_before = explore_robot->get_motion_handler()->get_velocity();
+  csci3081::WheelVelocity vc_before = coward_robot->get_motion_handler()->get_velocity();
+  
+  light1->set_pose(Lposeafter);
+  explore_robot->get_left_light_sensor()->update(light_entities_);
+  explore_robot->get_right_light_sensor()->update(light_entities_);
+  coward_robot->get_left_light_sensor()->update(light_entities_);
+  coward_robot->get_right_light_sensor()->update(light_entities_);
+  explore_robot->TimestepUpdate(1);
+  coward_robot->TimestepUpdate(1);
+
+  csci3081::WheelVelocity ve_after = explore_robot->get_motion_handler()->get_velocity();
+  csci3081::WheelVelocity vc_after = coward_robot->get_motion_handler()->get_velocity();
+
+  EXPECT_LT(ve_before.left,ve_after.left);
+  EXPECT_LT(ve_before.right,ve_after.right);
+  EXPECT_GT(vc_before.left,vc_after.left);
+  EXPECT_GT(vc_before.right,vc_after.right);
+  
+}
 
 #endif /*MOTIONHANDLER_TESTS*/
